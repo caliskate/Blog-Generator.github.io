acc7d32bbbe8f61039f0f3508bf7f337c580dc26
caliskate <dlverduzco@ucdavis.edu>
2024-07-03 21:37:20 -0700

    Updates blog - fixed module py files

--- Auto Blog Post Scripts/openai_utils.py
+++ Auto Blog Post Scripts/openai_utils.py
@@ -1,40 +1,69 @@
+from ast import main
+import shutil
 import os
 import requests
-import shutil
-
+import openai
 from openai import OpenAI
+from datetime import datetime as dt
+from PIL import Image 
 
-os.environ["OPENAI_API_KEY"] = input('enter your OpenAI API key: ')
+from getpass import getpass
 
+
+import openai #aiutils
+from openai import OpenAI #aiutils
+import os #aiutils
+from git import Repo #blog utils
+#used for setting up paths and parent dir to github repository
+from pathlib import Path #main and blogutil
+import shutil
+from bs4 import BeautifulSoup as Soup #blogutils
+from PIL import Image #aiutils
+from datetime import datetime as dt #aiutils -  for formatting date returned with images
+import requests #aiutils - for downloading images from URLs
+
+#openai utils = login,api calls, text, title and image saving
+#something in this is causing none type error on glob utils
+
+
+#user login
+inputuser=input('enter your OpenAI username: ')
+os.environ["OPENAI_API_KEY"] = getpass('Paste your OpenAI API key and press enter (no text will show) : ')
+
+
+
+#gpt api call
 client = OpenAI()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#gpt prompt creation
+def create_prompt(title):
+    prompt="""
+Biography:
+My name is Darin and I am a Python student.
+
+Blog
+Title: Python and the Future of AI
+tags: tech, python, coding, AI, machine learning
+Summary: I learn about what Python can hold for the future of AI. Please do not exceed 2 paragraphs in total response length.
+Full text: """.format(title) #070224 added 'Do not exceed 2 paragraphs.' based on max token length of 250
+    return prompt
+
 
 def system_prompt():
     return "You are a helpful assistant to write blogposts."
-
-
-
-
-
-
-
-
-
-
-def create_prompt(title):
-    prompt = """Jose's Website
- 
- Biography
- I am a Python instructor teaching people machine learning!
-
-
- Blog
-
- Jan 31, 2023
- Title: Why AI will never replace the radiologist
- tags: tech, machine-learning, radiology
- Summary:  I talk about the cons of machine learning in radiology. I explain why I think that AI will never replace the radiologist.
- Full text:""".format(title)
-    return prompt
 
 def get_blog_from_openai(blog_title):
     response = client.chat.completions.create(
@@ -49,9 +78,14 @@ def get_blog_from_openai(blog_title):
 
     return response.choices[0].message.content
 
+
+
+
+
+#image title
 def dalle3_prompt(title):
-    prompt = f"Pixel art showing '{title}'."
-    return prompt
+    prompt=f"Sci-fi art showing a movie scene with '{title}'. Extremely detailed and mesmerizing."
+    return prompt #added extremely detailed and mesmerizing 070224
 
 
 
@@ -71,16 +105,25 @@ def save_image(image_url, file_name):
     return image_res.status_code, file_name
 
 
+
+#dalle image api call - ok
 def get_cover_image(title, save_path):
-    response = client.images.generate(
-                    model="dall-e-3",
-                    prompt=dalle3_prompt(title),
-                    size="1024x1024",
-                    quality="standard",
-                    n=1,
+    images_response = client.images.generate(
+                    model= "dall-e-3", 
+                    n= 1,               # Between 2 and 10 is only for DALL-E 2
+                    size= "1024x1024",  # 256x256, 512x512 only for DALL-E 2 - not much cheaper - 1024x1024, 1024x1792 or 1792x1024 available for DALL-E 3
+                    prompt=dalle3_prompt(title),     # DALL-E 3: max 4000 characters, DALL-E 2: max 1000
+                    user= inputuser,      # pass a customer ID to OpenAI for abuse monitoring
+                    quality="standard"
                     )
-    image_url = response.data[0].url
+    image_url = images_response.data[0].url
     status_code, file_name = save_image(image_url, save_path)
     return status_code, file_name
+
+
+
+
+
+
 
 
